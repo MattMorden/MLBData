1b5f21e35dcb6ac95097c5b4da41eab89a040a6e
MattMorden <mattymorden@gmail.com>
2016-11-30 17:22:30 -0500

    Base Commit

---
+++ MLBData/MLBData/Index.aspx
@@ -0,0 +1,51 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="MLBData.Index" %>
+
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title></title>
+	<meta charset="utf-8" />
+    <script src="scripts/MLBScripts.js"></script>
+    <link href="Styles/MLBStyles.css" rel="stylesheet" />
+</head>
+<body>
+    <!--Create three html text boxes for Year, Month, Day-->
+    <!--Also give each box a default value to return game data-->
+
+
+    <form id="form">
+        <div id="myForm">
+            <h1>Matt Morden's Project 2: MLB data with JSON</h1>
+            <div id="yeararea">
+                <label>
+                    <span>Year:</span><input id="yearbox" type="text" name="year" value="2011" />
+                </label>
+            </div>
+
+            <div id="montharea">
+                <label>
+                    <span>Month:</span><input id="monthbox" type="text" name="month" value="07" />
+                </label>
+            </div>
+
+
+            <div id="dayarea">
+                <label>
+                    <span>Day:</span><input id="daybox" type="text" name="day" value="09" />
+                </label>
+            </div>
+
+        </div>
+        
+        <div id="buttonarea">
+            <input type="button" id="myButton" value="Get Baseball Data" onclick="getBaseballData()" />
+        </div>
+        
+    </form>
+
+    <!--Output is to be a list of games played for a given year/month/day and displayed using DHTML-->
+    <!--The details should include home team, and away team for each game, with Blue Jays games showing first in the list.-->
+
+</body>
+</html>
---
+++ MLBData/MLBData/scripts/MLBScripts.js
@@ -0,0 +1,100 @@
+﻿//Javascript for MLB
+
+var asynch = false;
+var baseballObject;
+var divHTML = "<table id='table'>";;
+
+// ***********************************
+// AJAX XMLHttpRequest to get the JSON
+// from the site defined by url
+function getJSON(url) {
+    var xmlHttp;
+
+    xmlHttp = new XMLHttpRequest();
+
+    if (xmlHttp !== null) {
+        if (asynch) {
+            // HTTP asynch GET request
+            xmlHttp.open("GET", url, true);
+            xmlHttp.onload = function (e) {
+                if (xmlHttp.status === 200) {
+                    procJSONRequest(xmlHttp.responseText);
+                }
+            }
+            xmlHttp.send(null);
+        }
+        else {
+            // HTTP synchronous GET request
+            xmlHttp.open("GET", url, false);
+            xmlHttp.send(null);
+            procJSONRequest(xmlHttp.responseText);
+        }
+    }
+}
+
+// ***********************************************
+// single function to process returned JSON string
+function procJSONRequest(resp) {
+
+
+    baseballObject = JSON.parse(resp);
+    //document.write(resp);
+    console.log(baseballObject);
+
+    if (baseballObject.data.games.game == undefined) {
+        console.log("NO DATA RETURNED");
+        divHTML += "<tr><td>NO DATA RETURNED FOR SELECTED DAY/MONTH/YEAR</td></tr>";
+    }
+    else {
+        for (var i = 0; i < baseballObject.data.games.game.length; i++) {
+            if (baseballObject.data.games.game[i].home_team_name === "Blue Jays" ||
+                baseballObject.data.games.game[i].away_team_name === "Blue Jays") {
+                console.log("Home team: " + baseballObject.data.games.game[i].home_team_name +
+                " and Away team: " + baseballObject.data.games.game[i].away_team_name);
+                divHTML += "<tr><td>";
+                divHTML += "Home Team: " + baseballObject.data.games.game[i].home_team_name + " and Away Team: " + baseballObject.data.games.game[i].away_team_name;
+                divHTML += "</tr></td>";
+            }
+        }
+
+
+        for (var i = 0; i < baseballObject.data.games.game.length; i++)
+            if (baseballObject.data.games.game[i].home_team_name !== "Blue Jays" ||
+                baseballObject.data.games.game[i].away_team_name !== "Blue Jays") {
+                console.log("Home team: " + baseballObject.data.games.game[i].home_team_name + " and Away team: " +
+                    baseballObject.data.games.game[i].away_team_name);
+                divHTML += "<tr><td>";
+                divHTML += "Home Team: " + baseballObject.data.games.game[i].home_team_name + " and Away Team: " + baseballObject.data.games.game[i].away_team_name;
+                divHTML += "</tr></td>";
+            }
+
+        divHTML += "</table>";
+    }//end else
+}
+
+
+
+
+// ************************************
+// onload event handler creates the URL
+// for a given year month and day
+function getBaseballData() {
+    var year = document.getElementById('yearbox').value;
+    var month = document.getElementById('monthbox').value;
+    var day = document.getElementById('daybox').value;
+
+    // inspect URL
+    var tempURL = "http://gd2.mlb.com/components/game/mlb/year_" + year + "/month_" + month + "/day_" + day + "/master_scoreboard.json";
+
+    getJSON("http://gd2.mlb.com/components/game/mlb/year_" + year + "/month_" + month + "/day_" + day + "/master_scoreboard.json");
+    createTable();
+}
+
+
+function createTable() {
+    document.getElementById("form").innerHTML = addTable();
+}
+
+function addTable() {
+    return divHTML;
+}
